30c1a673d9417e1aa7d901750a298468540a8820
Plaban Biswas <plabanbiswas96@outlook.com>
2025-01-28 16:03:21 -0500

    - ADDED: Sort and Timer Metrics

--- Assignment_2/Assignment_2/Main.cpp
+++ Assignment_2/Assignment_2/Main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include "Account.h"
+#include "Timer.h"
 
 void assignmentTask1();
 Assignment_2::Account** create_accounts(int size, int max_balance);
 void print_accounts(Assignment_2::Account* accounts[], int size);
-void sort_accounts(Assignment_2::Account* accounts[], int size);
+void sort_accounts(Assignment_2::Account** accounts, int size);
 void test_account();
+int insertion_sort(Assignment_2::Account* accounts[], int n);
 
 int main()
 {
@@ -41,10 +44,10 @@ void test_account()
 	std::cout << "\nPrinting the list of accounts:\n";
 	print_accounts(accounts, SIZE);
 
-	/*std::cout << "\nSorting the list of accounts:\n";
-	sort_accounts(accounts, SIZE);*/
+	std::cout << "\nSorting the list of accounts:\n";
+	sort_accounts(accounts, SIZE);
 
-	std::cout << "\nPrinting the list of accounts:\n";
+	std::cout << "\nPrinting the list of accounts after sort:\n";
 	print_accounts(accounts, SIZE);
 
 	std::cout << "\nDeleting the accounts:\n";
@@ -56,7 +59,7 @@ void test_account()
 
 Assignment_2::Account** create_accounts(int size, int max_balance)
 {
-	Assignment_2::Account** accounts = new Assignment_2::Account* [size];
+	Assignment_2::Account** accounts = new Assignment_2::Account * [size];
 
 	for (int i = 0; i < size; i++)
 	{
@@ -74,10 +77,35 @@ void print_accounts(Assignment_2::Account* accounts[], int size)
 	}
 }
 
-void sort_accounts(Assignment_2::Account* accounts[], int size)
+void sort_accounts(Assignment_2::Account** accounts, int size)
 {
-	for (int i = 0; i < size; i++)
+	Timer* timer = new Timer();
+	int swaps = insertion_sort(accounts, size);
+	delete(timer);
+
+	std::cout << "\nNo. of swaps: " << swaps << "\n";
+}
+
+int insertion_sort(Assignment_2::Account* accounts[], int size)
+{
+	int swaps = 0;
+
+	for (int i = 1; i < size; ++i)
 	{
-		accounts[i]->print_info();
+		Assignment_2::Account* key = accounts[i];
+
+		int j = i - 1;
+
+		while (j >= 0 && accounts[j]->get_balance() > key->get_balance())
+		{
+			accounts[j + 1] = accounts[j];
+			swaps++;
+			j = j - 1;
+		}
+
+		accounts[j + 1] = key;
+		swaps++;
 	}
+
+	return swaps;
 }
