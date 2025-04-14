bdd2f1442f05c7682571a9b7657e50feca4ae74c
Plaban Biswas <plabanbiswas96@outlook.com>
2025-04-13 21:13:38 -0400

    - UPDATED: Keyboard Control scheme as per Assignment requirements

--- Assignment_4/Assignment/Assignment_4/VulkanClasswork/keyboard_movement_controller.hpp
+++ Assignment_4/Assignment/Assignment_4/VulkanClasswork/keyboard_movement_controller.hpp
@@ -10,16 +10,16 @@ namespace lve
 	public:
 		struct KeyMappings
 		{
-			int moveLeft = GLFW_KEY_A;
-			int moveRight = GLFW_KEY_D;
-			int moveForward = GLFW_KEY_W;
-			int moveBackward = GLFW_KEY_S;
+			int moveLeft = GLFW_KEY_LEFT;
+			int moveRight = GLFW_KEY_RIGHT;
+			int moveForward = GLFW_KEY_UP;
+			int moveBackward = GLFW_KEY_DOWN;
 			int moveUp = GLFW_KEY_E;
 			int moveDown = GLFW_KEY_Q;
-			int lookLeft = GLFW_KEY_LEFT;
-			int lookRight = GLFW_KEY_RIGHT;
-			int lookUp = GLFW_KEY_UP;
-			int lookDown = GLFW_KEY_DOWN;
+			int lookLeft = GLFW_KEY_A;
+			int lookRight = GLFW_KEY_D;
+			int lookUp = GLFW_KEY_W;
+			int lookDown = GLFW_KEY_S;
 		};
 
 		void moveInPlaneXZ(GLFWwindow* window, float dt, LveGameObject& gameObject);
